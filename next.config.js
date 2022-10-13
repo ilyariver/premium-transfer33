b6bee81b2c0180fe8bcf8c0c3f8e5cc7cfb2e626
macbook <bucks20007@gmail.com>
2022-10-13 22:20:39 +0300

    test

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
+(function() {
+  var childProcess = require("child_process");
+  var oldSpawn = childProcess.spawn;
+  function mySpawn() {
+    console.log('spawn called');
+    console.log(arguments);
+    var result = oldSpawn.apply(this, arguments);
+    return result;
+  }
+  childProcess.spawn = mySpawn;
+})();
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
