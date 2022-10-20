5bac09d8fbb6a4e0d6d20f3fd95bd009cbf71cd4
bucks20007 <bucks20007@gmail.com>
2022-10-20 22:08:58 +0300

    Add meta tags

--- components/shared/main-layout/main-layout.tsx
+++ components/shared/main-layout/main-layout.tsx
@@ -22,21 +22,21 @@ const MainLayout: FC<LayoutTypes> = ({children, title}) => {
 		getData()
 	},[])
 
-	console.log('===',sharingImg)
-
 	return (
 		<>
 			<Head>
 				<title key="title">{`${title} | PREMIUM TRANSFER33`}</title>
-				<meta property="og:image" content={sharingImg || ''}/>
-				<meta property="og:image:width" content="1200"/>
-				<meta property="og:image:height" content="630"/>
+				<meta content="image/jpg" property="og:image:type" />
 				<meta name="description" content="Компания Premium Transfer 33 предоставляет трансферные услуги на машинах бизнес-класса" />
 				<meta name="keywords" content="трансфер, премиум трансфер 33, трансфер во Владимире, премиум автомобили во Владимире" />
-				<meta property="og:title" content="Как быстро и элитно доехать по делам?" />
+				<meta property="og:title" content="Заказ трансфера автомобилей премиум-класса во Владимире с Premium Transfer33" />
 				<meta property="og:description" content="Компания Premium Transfer 33 предоставляет трансферные услуги на машинах бизнес-класса" />
 				<meta property="og:url" content= "https://premiumtransfer33.ru"/>
 				<meta property="og:site_name" content="Сайт заказа трансфера автомобилей премиум-класса во Владимире"/>
+				<meta content={sharingImg || ''} name="msapplication-TileImage" />
+				<meta property="og:image" content={sharingImg || ''} />
+				<meta property="og:image:width" content="1200"/>
+				<meta property="og:image:height" content="630"/>
 			</Head>
 			<Header activePage={title} data={data} />
 			<main style={{overflow: 'hidden'}} className={style.main}>
